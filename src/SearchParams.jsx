7e063ef2ba90e01c2f860c331d277b23f03c0bba
mohamedaliznidi <mohamedali.znidi1@esprit.tn>
2021-04-20 02:20:17 +0200

    first commit

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 function SearchParams() {
-  const location = "Seatle, WA";
+  const [location, setLocation] = useState("Seatle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
+
   return (
     <div className="search-params">
+      <h1>{location}</h1>
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placehomder="location" />
+          <input
+            id="location"
+            value={location}
+            placehomder="location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
